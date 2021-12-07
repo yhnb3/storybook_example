891c43c8a841527696b7b6b13070b85a06691f21
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-12-07 21:20:00 +0900

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+};
